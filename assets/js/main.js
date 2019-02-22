874e3273db347516ac55fad1aceef6954c20b327
Igor Vieira <igor.p.r.vieira@gmail.com>
2019-02-22 03:24:33 -0300

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var video = document.getElementById("myVideo");
+var audio = document.getElementById("myAudio");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
